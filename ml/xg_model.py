0c400764eb10fe2d85c42ae1041b0fa39f733a99
Mateusz Michalski <matekmich@gmail.com>
2026-04-20 17:45:10 +0200

    feat: logistic regression for xg

--- ml/xg_model.py
+++ ml/xg_model.py
@@ -1,11 +1,15 @@
 import psycopg
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
 query = """
     SELECT shots.event_id, events.x, events.y, shots.outcome
     FROM shots
     JOIN events ON shots.event_id = events.event_id;
 """
+
 with psycopg.connect("dbname=statsbomb user=sb password=sbpass host=localhost") as conn:
     df = pd.read_sql_query(query, conn)
 
@@ -17,4 +21,25 @@ with psycopg.connect("dbname=statsbomb user=sb password=sbpass host=localhost") 
 
     df['angle'] = np.abs(kąt_slupek1 - kąt_slupek2) * (180 / np.pi)
 
-    print(df[['outcome', 'is_goal', 'distance', 'angle']].head())
+    #print(df[['outcome', 'is_goal', 'distance', 'angle']].head())
+    
+    X = df[['distance', 'angle']]
+    y = df['is_goal']
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y,
+        test_size=0.2,
+        random_state=69,
+        shuffle=True
+    )
+    #print(X_train,X_test,len(X_train),len(X_test))
+
+    cristiano_modaldo = model = LogisticRegression()
+    cristiano_modaldo.fit(X_train,y_train)
+    xg_predictions = cristiano_modaldo.predict_proba(X_test)[:, 1]
+    
+    results = X_test.assign(
+    goal_scored=y_test.values,
+    xG=xg_predictions
+    )
+    print(results.round(3))
