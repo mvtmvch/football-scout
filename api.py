5711c7604cf84403605cd193062313be59380370
Mateusz Michalski <matekmich@gmail.com>
2026-04-28 12:40:30 +0200

    feat: add team and player report endpoints

--- api.py
+++ api.py
@@ -1,6 +1,8 @@
+from pathlib import Path
 from fastapi import FastAPI
 import joblib
 import pandas as pd
+import psycopg
 
 app =  FastAPI()
 model = joblib.load('ml/model_xg.joblib')
@@ -10,3 +12,17 @@ def count_xg(distance:float, angle:float):
     data=pd.DataFrame([{'distance' : distance, 'angle' : angle}])
     proba_goal = model.predict_proba(data)[0, 1]
     return {"xg": float(proba_goal)}
+
+@app.get("/teams/report")
+def show_report():
+    query = Path('sql/team_report.sql').read_text()
+    with psycopg.connect("dbname=statsbomb user=sb password=sbpass host=localhost") as conn:
+        df = pd.read_sql_query(query, conn)
+        return df.to_dict(orient="records")
+    
+@app.get("/players/report")
+def show_report():
+    query = Path('sql/player_report.sql').read_text()
+    with psycopg.connect("dbname=statsbomb user=sb password=sbpass host=localhost") as conn:
+        df = pd.read_sql_query(query, conn)
+        return df.to_dict(orient="records")
